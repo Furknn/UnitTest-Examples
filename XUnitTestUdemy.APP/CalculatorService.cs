020445bc7ef04600600bbcdcb732e206f63b9601
Furknn <fs.2838@gmail.com>
2022-07-23 19:09:44 +0300

    Moq verify and throws methods

--- XUnitTestUdemy.APP/CalculatorService.cs
+++ XUnitTestUdemy.APP/CalculatorService.cs
@@ -1,21 +1,25 @@
 namespace XUnitTestUdemy.APP;
 
-public class CalculatorService:ICalculatorService
+public class CalculatorService : ICalculatorService
 {
     public int Add(int a, int b)
     {
-        //  if (a==0 || b==0)
-        //  {
-        //      return 0;
-        //  }
-        //  
-        //  return a + b;
-        return 0;
+        if (a == 0 || b == 0)
+        {
+            return 0;
+        }
+
+        return a + b;
     }
 
     public int Multiply(int a, int b)
     {
-        return 5;
+        if (a == 0)
+        {
+            throw new Exception("a=0 olamaz");
+        }
+
+        return a * b;
     }
 }
 
--- XUnitTestUdemy.TEST/CalculatorTest.cs
+++ XUnitTestUdemy.TEST/CalculatorTest.cs
@@ -1,3 +1,4 @@
+using System;
 using Moq;
 using Xunit;
 using XUnitTestUdemy.APP;
@@ -8,6 +9,7 @@ public class CalculatorTest
 {
     private Calculator _calculator { get; set; }
     private Mock<ICalculatorService> mymock { get; set; }
+
     public CalculatorTest()
     {
         mymock = new Mock<ICalculatorService>();
@@ -15,35 +17,44 @@ public class CalculatorTest
     }
 
     [Theory]
-    [InlineData(5,20,25)]
-    [InlineData(10,2,12)]
-    public void Add_simpleValues_ReturnTotalValue(int a,int b,int expectedTotal)
+    [InlineData(5, 20, 25)]
+    [InlineData(10, 2, 12)]
+    public void Add_simpleValues_ReturnTotalValue(int a, int b, int expectedTotal)
     {
-        
         mymock.Setup(x => x.Add(a, b)).Returns(expectedTotal);
         var actualTotal = _calculator.Add(a, b);
-        
-        // Assert
         Assert.Equal(expectedTotal, actualTotal);
+        mymock.Verify(x => x.Add(a, b), Times.Once);
     }
-    
+
     [Theory]
-    [InlineData(0,5,0)]
-    [InlineData(10,0,0)]
+    [InlineData(0, 5, 0)]
+    [InlineData(10, 0, 0)]
     public void Add_zeroValues_ReturnTotalValue(int a, int b, int expectedTotal)
     {
         mymock.Setup(x => x.Add(a, b)).Returns(expectedTotal);
         var actualTotal = _calculator.Add(a, b);
         Assert.Equal(actualTotal, expectedTotal);
+        mymock.Verify(x => x.Add(a, b), Times.Once);
     }
-    
+
     [Theory]
-    [InlineData(5,20,120)]
-    [InlineData(10,2,20)]
-    public void Multiply_simpleValues_ReturnTotalValue(int a,int b,int expectedTotal)
+    [InlineData(5, 20, 120)]
+    [InlineData(10, 2, 20)]
+    public void Multiply_simpleValues_ReturnTotalValue(int a, int b, int expectedTotal)
     {
         mymock.Setup(x => x.Multiply(a, b)).Returns(expectedTotal);
         var actualMult = _calculator.Multiply(a, b);
         Assert.Equal(actualMult, expectedTotal);
+        mymock.Verify(x => x.Multiply(a, b), Times.Once);
+    }
+
+    [Theory]
+    [InlineData(0, 5)]
+    public void Multiply_zeroValues_ReturnsException(int a, int b)
+    {
+        mymock.Setup(x => x.Multiply(a, b)).Throws(new Exception("a=0 olamaz"));
+        var exception= Assert.Throws<Exception>(() => _calculator.Multiply(a, b));
+        Assert.Equal("a=0 olamaz", exception.Message);
     }
 }
